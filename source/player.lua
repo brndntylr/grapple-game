c7656440f541b1ebe44376f284579b818e2c557d
brndntylr <brndntylr94@outlook.com>
2025-03-12 23:15:54 +0000

    fixing player collision and adding platform to globals

--- source/player.lua
+++ source/player.lua
@@ -11,13 +11,14 @@ function Player:init(x, y, speed, jumpspeed, gravity)
 	Player.super.init(self)
 
     self.speed = speed
-    self.jumpspeed = jumpspeed
+    self.yspeed = 0
     self.gravity = gravity
 
 	-- Currently just a single image for the sprite but will add animation in the future
 	local player_image = gfx.image.new("images/dude-0001.png")
     self:setImage(player_image)
-	self:setCollideRect(4, 4, 24, 40)
+	self:setCollideRect(0, 0, self:getSize())
+    self:setCollidesWithGroups(1)
 	self:setZIndex(10)
     self:moveTo(x, y)
 end
@@ -26,55 +27,27 @@ function Player:update()
 
     Player.super.update(self)
 
-    -- local player_pos_x  = self.x
-
     if pd.buttonIsPressed(pd.kButtonRight) then
-        self:moveBy(self.speed, 0)
+        self:moveWithCollisions(self.x+self.speed, self.y)
+        -- print(pos_x+self.speed)
+        -- self:moveTo(pos_x+self.speed, 0)
     end
     if pd.buttonIsPressed(pd.kButtonLeft) then
-        self:moveBy(-self.speed, 0)
+        self:moveWithCollisions(self.x-self.speed, self.y)
+        -- print(pos_x-self.speed)
+        -- self.moveTo(pos_x-self.speed, 0)
     end
     if pd.buttonJustPressed(pd.kButtonA) then
-        self.jumpspeed = -10
+        self.yspeed = -
     end
 
-    self:moveBy(0,self.jumpspeed)
+    self:moveWithCollisions(self.x,self.y+self.jumpspeed)
 
-    local player_pos_y  = self.y
-
-    if player_pos_y < ground_y then
-        self.jumpspeed += self.gravity
-    elseif player_pos_y >= ground_y then
-        self:moveBy(0, ground_y-player_pos_y)
-        self.jumpspeed = 0
-    end
+    self.yspeed += self.gravity
 end
 
-
--- function Player:reset()
--- 	-- self.xd = 0
--- 	-- self.yd = 0
--- 	-- self.v = 0
-
--- 	self.frame = 0
--- 	self:updateFrame()
-
--- 	self:moveTo(playdate.display.getWidth() / 4, playdate.display.getHeight() / 4)
--- end
-
-
-
--- function Player:up()
-
--- end
-
-
--- function Player:left()
-
--- end
-
-
--- function Player:right()
-
--- end
-
+function Player:collisionResponse(other)
+    if other.super.className == "Platform" then
+        return "slide"
+    end
+end
