b430d936dba0333beea9977dd2fa47514732e28d
brndntylr <brndntylr94@outlook.com>
2025-03-12 23:09:09 +0000

    Import platform and set some to test

--- source/main.lua
+++ source/main.lua
@@ -3,21 +3,25 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "player"
--- import "grapple"
 import "platform"
+-- import "grapple"
 -- import "grapple_surface"
 
 playdate.display.setRefreshRate(30)
 
 local pd = playdate
 local gfx = pd.graphics
--- local spritelib = gfx.sprite
--- local screenWidth = pd.display.getWidth()
--- local screenHeight = pd.display.getHeight()
 
 local function initialise()
     local player_sprite = Player(200, 120, 2, 6, 1)
     player_sprite:add()
+
+    local platform_001 = Platform(200, 230, 400, 4)
+    platform_001:add()
+
+    local platform_002 = Platform(300, 200, 100, 4)
+    platform_002:add()
+    
 end
 
 initialise()
